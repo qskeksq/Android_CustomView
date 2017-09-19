933e509c1625858d98c3226ea99f82863c599956
Administrator <qskeksq@gmail.com>
2017-09-20 02:21:47 +0900

    readme

--- app/src/main/java/com/example/administrator/customview3/MainActivity.java
+++ app/src/main/java/com/example/administrator/customview3/MainActivity.java
@@ -2,65 +2,59 @@ package com.example.administrator.customview3;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-
-import com.example.administrator.customview3.view.RainBowProgress;
-import com.example.administrator.customview3.view.RainbowDivider;
 
 public class MainActivity extends AppCompatActivity {
     Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_measuring);
+        setContentView(R.layout.activity_piano);
 //
-        final RainBowProgress rainBowProgress = (RainBowProgress) findViewById(R.id.rainbow);
-        final RainbowDivider divider = (RainbowDivider) findViewById(R.id.divider);
-
-        Button button = (Button) findViewById(R.id.button6);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                handler.sendEmptyMessage(0);
-            }
-        });
-
-        Button button2 = (Button) findViewById(R.id.button7);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                handler.sendEmptyMessage(1);
-            }
-        });
-
-        handler = new Handler(){
-            @Override
-            public void handleMessage(Message msg) {
-                super.handleMessage(msg);
-                switch (msg.what){
-                    case 0:
-                        int curPos = rainBowProgress.getCurPos();
-                        if(curPos < rainBowProgress.getMAX()){
-                            rainBowProgress.setCurPos(curPos+1);
-                            handler.sendEmptyMessageDelayed(0, 10);
-                        } else {
+//        final RainBowProgress rainBowProgress = (RainBowProgress) findViewById(R.id.rainbow);
+//        final RainbowDivider divider = (RainbowDivider) findViewById(R.id.divider);
+//
+//        Button button = (Button) findViewById(R.id.button6);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                handler.sendEmptyMessage(0);
+//            }
+//        });
+//
+//        Button button2 = (Button) findViewById(R.id.button7);
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                handler.sendEmptyMessage(1);
+//            }
+//        });
+//
+//        handler = new Handler(){
+//            @Override
+//            public void handleMessage(Message msg) {
+//                super.handleMessage(msg);
+//                switch (msg.what){
+//                    case 0:
+//                        int curPos = rainBowProgress.getCurPos();
+//                        if(curPos < rainBowProgress.getMAX()){
+//                            rainBowProgress.setCurPos(curPos+1);
+//                            handler.sendEmptyMessageDelayed(0, 10);
+//                        } else {
 //                            rainBowProgress.setCurPos(0);
-                        }
-                        break;
-                    case 1:
-                        int curPos2 = divider.getCurPos();
-                        if(curPos2 < divider.getMAX()){
-                            divider.setCurPos(curPos2+1);
-                            handler.sendEmptyMessageDelayed(1, 10);
-
-                        }
-                        break;
-                }
-            }
-        };
+//                        }
+//                        break;
+//                    case 1:
+//                        int curPos2 = divider.getCurPos();
+//                        if(curPos2 < divider.getMAX()){
+//                            divider.setCurPos(curPos2+1);
+//                            handler.sendEmptyMessageDelayed(1, 10);
+//
+//                        }
+//                        break;
+//                }
+//            }
+//        };
 
     }
 }
--- app/src/main/java/com/example/administrator/customview3/view/Piano.java
+++ app/src/main/java/com/example/administrator/customview3/view/Piano.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -17,9 +20,11 @@ import android.view.View;
 public class Piano extends View {
 
     // 건반 수, 간격
+    private int MAX_POINTS = 5;
     private int WHITE_KEY_NUM = 7;
     private int BLACK_KEY_NUM = 5;
     private float KEY_PADDING = 0.02F;
+    private float BLACK_KEY_PADDING = 0.5f;
     private Rect[] whiteKeys = new Rect[WHITE_KEY_NUM];
     private Rect[] blackKeys = new Rect[BLACK_KEY_NUM];
 
@@ -32,9 +37,14 @@ public class Piano extends View {
     private Paint[] blackPaints = new Paint[BLACK_KEY_NUM];
 
     // 실제 크기
-    int width, keyWidth;
-    int height;
+    int width, keyWidth, blackWidth, blackKeyWidth;
+    int height, blackHeight;
     int padding, paddings;
+
+    // 포인터
+    private Point point;
+    private Point[] points = new Point[MAX_POINTS];
+    private MotionEvent.PointerCoords pointerCoords;
 
     public Piano(Context context) {
         super(context);
@@ -81,11 +91,20 @@ public class Piano extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
+
         width = getDefaultSize(getSuggestedMinimumHeight(), widthMeasureSpec);
         height = getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+
+        // 흰 건반
         padding = ((int) (width*KEY_PADDING))/(WHITE_KEY_NUM+1);
         paddings = (int) (width*KEY_PADDING);
         keyWidth = (width-paddings)/(WHITE_KEY_NUM);
+
+        // 검정건반
+        blackWidth = (int) ((width-paddings)*BLACK_KEY_PADDING);
+        blackKeyWidth = blackWidth/BLACK_KEY_NUM;
+        blackHeight = (int) (height*BLACK_KEY_PADDING);
+
 
         setMeasuredDimension(width, height);
     }
@@ -108,6 +127,37 @@ public class Piano extends View {
             rect.bottom = height-getPaddingBottom();
             rect.right = padding*(column+1) + keyWidth*(column+1);
         }
+        int WHITE_TO_BLACK = keyWidth-blackKeyWidth/2-padding/2;
+
+        Rect rect = blackKeys[0];
+        rect.left = padding+WHITE_TO_BLACK+keyWidth*0;
+        rect.top = getPaddingTop();
+        rect.bottom = getPaddingTop()+blackHeight;
+        rect.right = rect.left+blackKeyWidth;
+
+        rect = blackKeys[1];
+        rect.left = padding*2+WHITE_TO_BLACK+keyWidth*1;
+        rect.top = getPaddingTop();
+        rect.bottom = getPaddingTop()+blackHeight;
+        rect.right = rect.left+blackKeyWidth;
+
+        rect = blackKeys[2];
+        rect.left = padding*4+WHITE_TO_BLACK+keyWidth*3;
+        rect.top = getPaddingTop();
+        rect.bottom = getPaddingTop()+blackHeight;
+        rect.right = rect.left+blackKeyWidth;
+
+        rect = blackKeys[3];
+        rect.left = padding*5+WHITE_TO_BLACK+keyWidth*4;
+        rect.top = getPaddingTop();
+        rect.bottom = getPaddingTop()+blackHeight;
+        rect.right = rect.left+blackKeyWidth;
+
+        rect = blackKeys[4];
+        rect.left = padding*6+WHITE_TO_BLACK+keyWidth*5;
+        rect.top = getPaddingTop();
+        rect.bottom = getPaddingTop()+blackHeight;
+        rect.right = rect.left+blackKeyWidth;
     }
 
     /**
@@ -122,6 +172,64 @@ public class Piano extends View {
         for (int i = 0; i < WHITE_KEY_NUM; i++) {
             canvas.drawRect(whiteKeys[i], whitePaints[i]);
         }
+        for (int i = 0; i < BLACK_KEY_NUM; i++) {
+            canvas.drawRect(blackKeys[i], blackPaints[i]);
+        }
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int actionIndex = event.getActionIndex();
+        int action = event.getActionMasked();
+        int id = event.getPointerId(actionIndex);
+
+        switch (action){
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_POINTER_DOWN:
+                points[id] = new Point((int)event.getX(), (int)event.getY());
+                invalidateKey(points[id], true);
+                Log.e("down아이디", id+"");
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+            case MotionEvent.ACTION_CANCEL:
+                invalidateKey(points[id], false);
+                points[id] = null;
+                Log.e("up아이디", id+"");
+                break;
+        }
+        return true;
+    }
+
+    private void invalidateKey(Point point, boolean isDown){
+        Log.e("invalidateKey", "호출 확인1");
+        if(isDown) {
+            // 먼저 흰키 영역인지 확인하고
+            for (int i = 0; i < WHITE_KEY_NUM; i++) {
+                Rect rect = whiteKeys[i];
+                if (rect.contains(point.x, point.y)) {
+                    // 흰키 영역 안의 검정 영역이면
+                    for (int j = 0; j < BLACK_KEY_NUM; j++) {
+                        Rect blackRect = blackKeys[j];
+                        if (blackRect.contains(point.x, point.y)) {
+                            blackPaints[j].setColor(Color.CYAN);
+                            invalidate();
+                            return;
+                        }
+                    }
+                    Log.e("invalidateKey", "호출 확인2");
+                    whitePaints[i].setColor(Color.CYAN);
+                }
+            }
+        } else {
+            for (int i = 0; i < WHITE_KEY_NUM; i++) {
+                whitePaints[i].setColor(Color.WHITE);
+            }
+            for (int i = 0; i < BLACK_KEY_NUM; i++) {
+                blackPaints[i].setColor(Color.BLACK);
+            }
+        }
+        invalidate();
     }
 }
